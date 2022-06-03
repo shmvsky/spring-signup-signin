73fcfb038977b68200212bc483f41c4c0dd7fad4
shmvsky <grrenshi1337@gmail.com>
2022-06-03 11:10:48 +0300

    static files + some application properties

---
+++ src/test/java/com/grengof/springsignupsignin/SpringSignupSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grengof.springsignupsignin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSignupSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
